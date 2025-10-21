9a7561495c404cf8ad11284cd6b56b45264432c7
nateeatsrice <nathaniel.c.rice@gmail.com>
2025-10-20 23:54:22 -0500

    Initial Commit

---
+++ Trekdar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trekdar
+//
+//  Created by Nathaniel Rice on 10/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trekdar/TrekdarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekdarApp.swift
+//  Trekdar
+//
+//  Created by Nathaniel Rice on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TrekdarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
